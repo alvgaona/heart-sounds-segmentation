97cd7accb18c7acc8a3578e058a97ebceb4f2fe7
Alvaro <alvgaona@gmail.com>
2023-09-03 00:59:12 -0300

    Update transforms
    - Adapt format
    - Add docstrings
    - Return original signal

--- hss/transforms/__init__.py
+++ hss/transforms/__init__.py
@@ -1,26 +1,63 @@
+from typing import Optional, Tuple
+
+import numpy
 import numpy as np
 import scipy
 import ssqueezepy as ssq
+import torch
+
 
 class Resample:
-  def __init__(self, num):
-    self._num = num
+    def __init__(self, num: int) -> None:
+        """
+        Args:
+            num (int): number of output samples
+        """
+        self.num = num
 
-  def __call__(self, x):
-    return scipy.signal.resample(x, self._num)
+    def __call__(self, x) -> np.ndarray:
+        """
+        Args:
+            x: input signal to get resampled
+
+        Returns:
+            (np.ndarray): resampled signal
+        """
+        return scipy.signal.resample(x, self.num)
+
 
 class FSST:
-  def __init__(self, fs, flipud=False, window=None):
-    self.flipud = flipud
-    self.fs = fs
-    self.window = window
+    """
+    Fourier Synchrosqueezed Transform
+    """
 
-  def __call__(self, x):
-    Tsx, Sx, f, *_ = ssq.ssq_stft(
-        x,
-        flipud=self.flipud,
-        fs=self.fs,
-        window=self.window,
-    )
+    def __init__(self, fs: float, flipud: bool = False, window: Optional[numpy.ndarray] = None):
+        """
+        Args:
+            fs (float): sample frequency
+            flipud (bool): ?. Default: False
+            window (numpy.ndarray): window provided to compute the transform. Default: None
+        """
+        self.flipud = flipud
+        self.fs = fs
+        self.window = window
 
-    return Tsx, Sx, np.ascontiguousarray(f)
+    def __call__(self, x: torch.Tensor) -> Tuple:
+        """
+        Computes the transform
+
+        Args:
+            x (torch.Tensor): input signal
+
+        Returns:
+            Tuple: Original signal, STFT Synchrosqueezed Transform, STFT, and frequencies
+
+        """
+        fsst, stft, f, *_ = ssq.ssq_stft(
+            x,
+            flipud=self.flipud,
+            fs=self.fs,
+            window=self.window,
+        )
+
+        return x, fsst, stft, np.ascontiguousarray(f)
