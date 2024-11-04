dd1326dc7afad4fb38aaad3e04ac3bda97ce46d1
Alvaro <alvgaona@gmail.com>
2024-11-03 21:35:49 -0300

    Make the model flexible for dynamic batch sizes

--- hss/model/segmenter.py
+++ hss/model/segmenter.py
@@ -7,22 +7,17 @@ class HeartSoundSegmenter(nn.Module):
         self,
         *,
         input_size: int,
-        batch_size: int = 1,
         hidden_size: int = 240,
         bidirectional: bool = True,
-        device: torch.device = torch.cpu,
+        device: torch.device | None = None,
         dtype: torch.dtype = torch.float32,
     ):
         super().__init__()
 
-        self.batch_size = batch_size
-
-        D = 2 if bidirectional else 1
-
-        self.h0, self.c0 = (
-            torch.randn(D, batch_size, hidden_size, device=device, dtype=dtype),
-            torch.randn(D, batch_size, hidden_size, device=device, dtype=dtype),
-        )
+        self.bidirectional = bidirectional
+        self.hidden_size = hidden_size
+        self.device = device if device else torch.device("cpu")
+        self.dtype = dtype
 
         self.lstm_1 = nn.LSTM(
             input_size=input_size,
@@ -32,6 +27,7 @@ class HeartSoundSegmenter(nn.Module):
             device=device,
             dtype=dtype,
         )
+
         self.lstm_2 = nn.LSTM(
             input_size=hidden_size * 2,
             hidden_size=hidden_size,
@@ -40,17 +36,41 @@ class HeartSoundSegmenter(nn.Module):
             device=device,
             dtype=dtype,
         )
+
         self.dropout = nn.Dropout(0.2)
         self.relu = nn.ReLU()
         self.linear = nn.Linear(in_features=hidden_size * 2, out_features=4, bias=True, device=device, dtype=dtype)
         self.softmax = nn.LogSoftmax(dim=2)
 
+    def _init_hidden(self, batch_size: int) -> tuple[torch.Tensor, torch.Tensor]:
+        D = 2 if self.bidirectional else 1
+        h0 = torch.randn(
+            D,
+            batch_size,
+            self.hidden_size,
+            device=self.device,
+            dtype=self.dtype,
+        )
+        c0 = torch.randn(
+            D,
+            batch_size,
+            self.hidden_size,
+            device=self.device,
+            dtype=self.dtype,
+        )
+        return h0, c0
+
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        output, (hn, cn) = self.lstm_1(x, (self.h0, self.c0))
+        h0, c0 = self._init_hidden(x.size(0))
+
+        output, (hn, cn) = self.lstm_1(x, (h0, c0))
+
         output = self.relu(output)
         output = self.dropout(output)
+
         output, _ = self.lstm_2(output, (hn, cn))
         output = self.relu(output)
         output = self.dropout(output)
+
         output = self.linear(output)
         return self.softmax(output)
